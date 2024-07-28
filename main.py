c0274a8a28748349c89d2e2b51e4130be32d59f0
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-28 13:47:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+from copy import deepcopy
+from random import choice, randrange
+
+W, H = 10, 20
+TILE = 45
+GAME_RES = W * TILE, H * TILE
+RES = 750, 940
+FPS = 60
+
+pygame.init()
+sc = pygame.display.set_mode(RES)
+game_sc = pygame.Surface(GAME_RES)
+clock = pygame.time.Clock()
+
+grid = [pygame.Rect(x * TILE, y * TILE, TILE, TILE) for x in range(W) for y in range(H)]
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
+field = [[0 for i in range(W)] for j in range(H)]
+
+
+bg = pygame.image.load('img/bg.jpg').convert()
+game_bg = pygame.image.load('img/bg2.jpg').convert()
+
+main_font = pygame.font.Font('font/font.ttf', 65)
+font = pygame.font.Font('font/font.ttf', 45)
+title_tetris = main_font.render('TETRIS', True, pygame.Color('darkorange'))
+title_score = font.render('score:', True, pygame.Color('green'))
+title_record = font.render('record:', True, pygame.Color('purple'))
+
+get_color = lambda: (randrange(30, 256), randrange(30, 256), randrange(30, 256))
+figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
+color, next_color = get_color(), get_color()
+
+score, lines = 0, 0
+scores = {0: 0, 1: 100, 2: 300, 3: 700, 4: 1500}
+
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
+        return False
+    return True
